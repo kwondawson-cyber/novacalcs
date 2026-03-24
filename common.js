7d3347e566b30449f019824eb7a1b7587f0e7641
kwondawson-cyber <kwondawson@gmail.com>
2026-03-24 12:33:09 +0900

    Add files via upload

--- common.js
+++ common.js
@@ -3,6 +3,19 @@
    Favorites · Search · Utils
    ============================================ */
 
+// Google Analytics 4
+(function(){
+  var s = document.createElement('script');
+  s.async = true;
+  s.src = 'https://www.googletagmanager.com/gtag/js?id=G-1QPFT1SJF4';
+  document.head.appendChild(s);
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', 'G-1QPFT1SJF4');
+})();
+
 (function () {
   'use strict';
 
